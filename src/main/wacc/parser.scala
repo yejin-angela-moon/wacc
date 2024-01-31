d41ac1b48d062c5f11532127caeee58359b43d0a
ym1721 <ym1721@ic.ac.uk>
2024-01-31 22:38:28 +0000

    Expression contents moved into parser

--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -26,7 +26,7 @@ object Expression {
     // | ⟨array-elem⟩ | ‘(’ ⟨expr⟩ ‘)’
 
     private lazy val `<atom>`: Parsley[Expr] =
-        `<int-liter>`   <|>
+        `<int-liter>`   <|> 
         `<bool-liter>`  <|>
         `<char-liter>`  <|>
         `<str-liter>`
@@ -36,7 +36,7 @@ object Expression {
         // <|>  `<array-alem>`
 
 
-    private lazy val `<expr>`: Parsley[Expr] = precedence {
+    private lazy val `<expr>`: Parsley[Expr] = precedence (
         SOps(InfixR)(Or     from "||") +:
         SOps(InfixR)(And    from "&&") +:
         SOps(InfixN)(LT     from "<",   LTE     from "<=",
@@ -50,20 +50,64 @@ object Expression {
         SOps(Prefix)(Len    from "len", Ord     from "ord",
                      Chr    from "chr") +:
         Atoms(`<int-liter>`)
-    }
+    )
+
+    //     val binaryExpr: Parsley[Expr] = precedence(atom)(
+    //     InfixL(lexer.implicits.symbol("+") #> ((x: Expr, y: Expr) => Add(x, y))),
+    //     InfixL(lexer.implicits.symbol("-") #> ((x: Expr, y: Expr) => Sub(x, y))),
+    //     InfixL(lexer.implicits.symbol("*") #> ((x: Expr, y: Expr) => Mul(x, y))),
+    //     InfixL(lexer.implicits.symbol("/") #> ((x: Expr, y: Expr) => Div(x, y)))
+    // )
 }
 
 object parser {
+    import parsley.Parsley
+    import parsley.{Result, Success, Failure}
+    import Expression._
+    import lexer._
 
-    def parse(expr: String): Result[String, BigInt] = parser.parse(expr)
+    private lazy val `<int-liter>` : Parsley[IntLit]    = IntLit(INTEGER)
+    private lazy val `<bool-liter>`: Parsley[BoolLit]   = BoolLit(BOOL)
+    private lazy val `<char-liter>`: Parsley[CharLit]   = CharLit(CHAR)
+    private lazy val `<str-liter>` : Parsley[StrLit]    = StrLit(STRING)
 
-    private val parser = fully(expr)
+    private lazy val `<atom>`: Parsley[Expr] =
+        `<int-liter>`   <|> 
+        `<bool-liter>`  <|>
+        `<char-liter>`  <|>
+        `<str-liter>`
 
-    private val add = (x: BigInt, y: BigInt) => x + y
-    private val sub = (x: BigInt, y: BigInt) => x - y
+    private lazy val `<expr>`: Parsley[Expr] = precedence (
+        SOps(InfixR)(Or     from "||") +:
+        SOps(InfixR)(And    from "&&") +:
+        SOps(InfixN)(LT     from "<",   LTE     from "<=",
+                     GT     from ">",   GTE     from ">=",
+                     E      from "==",  NE      from "!=") +:
+        SOps(InfixL)(Mod    from "%") +:
+        SOps(InfixL)(Add    from "+",   Sub     from "-") +:
+        SOps(Prefix)(Neg    from "-") +:
+        SOps(Prefix)(Not    from "!") +:
+        SOps(InfixL)(Mul    from "*",   Div     from "/") +:
+        SOps(Prefix)(Len    from "len", Ord     from "ord",
+                     Chr    from "chr") +:
+        Atoms(`<int-liter>`)
+    )
 
-    private lazy val expr: Parsley[BigInt] =
-        chain.left1(INTEGER | "(" ~> expr <~ ")")(
-            ("+" as add) | ("-" as sub)
-        )
+
+    def parse(expr: String): Result[String, Expr] = {
+        fully(`<expr>`).parse(expr) match {
+            case Success(result) => Success(result)
+            case Failure(error) => Failure(error.toString)
+        }
+    }
+
+    // private val parser = fully(expr)
+
+    // private val add = (x: BigInt, y: BigInt) => x + y
+    // private val sub = (x: BigInt, y: BigInt) => x - y
+
+    // private lazy val expr: Parsley[BigInt] =
+    //     chain.left1(INTEGER | "(" ~> expr <~ ")")(
+    //         ("+" as add) | ("-" as sub) 
+    //     )
 }
