8553044c95a34bc3eb75c2197bbb1bf67d76d1e2
ms5022 <ms5022@ic.ac.uk>
2024-02-06 17:51:01 +0000

    layout of the semantic using the AST

--- src/main/wacc/semantic.scala
+++ src/main/wacc/semantic.scala
@@ -1,46 +1,109 @@
 package wacc
 
 import scala.collection.mutable.ListBuffer
+import parsley.{Result, Success, Failure}
+import ast._
+import CheckExprSemantic._
+import CheckStatmentSemantic._
+import CheckTypeSemantic._
 
-sealed trait Type
-case object IntType extends Type
-case object StringType extends Type
-case class ArrayType(elementType: Type, isFrozen: Boolean) extends Type
+object SemanticTable {
 
-sealed trait Expr
-case class Var(name: String, varType: Type) extends Expr
-case class ArrayLiteral(elements: List[Expr], elementType: Type, isFrozen: Boolean) extends Expr
-case class ArrayAccess(arr: Expr, index: Expr) extends Expr
-case class ArrayModification(arr: Expr, index: Expr, newValue: Expr) extends Expr
-
-class Semantic {
-def checkSemantic(expr: Expr, error: Error): Type = {
-  expr match {
-    case Var(_, varType) => varType
-    case ArrayLiteral(elements, elementType, isFrozen) =>
-      elements.foreach(checkSemantic(_, error))
-      ArrayType(elementType, isFrozen)
-    case ArrayAccess(arr, index) =>
-      val ArrayType(elementType, _) = checkSemantic(arr, error)
-      val indexType = checkSemantic(index, error)
-      if (indexType != IntType) {
-        error.addError("Array index must be of type Int.")
-      }
-      elementType
-    case ArrayModification(arr, index, newValue) =>
-      val ArrayType(elementType, isFrozen) = checkSemantic(arr, error)
-      val indexType = checkSemantic(index, error)
-      if (indexType != IntType) {
-        error.addError("Array index must be of type Int.")
-      }
-      val newValueType = checkSemantic(newValue, error)
-      if (isFrozen) {
-        error.addError("Cannot modify a frozen array.")
-      }
-      if (newValueType != elementType) {
-        error.addError("New value type does not match array element type.")
-      }
-      elementType
-  }
 }
+
+object CheckExprSemantic {
+
+    def checkExprSemantic(expr: Expr) : Result[String, Stmt] = {
+        expr match {
+            case Add => {}
+            case Sub => {}
+            case Mul => {}
+            case Div => {}
+            case Mod => {}
+            case And => {}
+            case Or => {}
+            case LT => {}
+            case LTE => {}
+            case GT => {}
+            case GTE => {}
+            case E => {}
+            case NE => {}
+            case Not => {}
+            case Neg => {}
+            case Len => {}
+            case Ord => {}
+            case Chr => {}
+            case default => checkLit(expr)
+        }
+    }
+
+    def checkLit (lit: Expr) : Result[String, Stmt] = {
+        lit match {
+            case IntLit => {}
+            case BoolLit => {}
+            case CharLit => {}
+            case StrLit => {}
+            case Ident => {}
+            case ArrayElem => {}
+            case Paran => {}
+            case PairLit => {}
+        }
+    }
+}
+
+object CheckStatmentSemantic {
+    def checkFunctionList(list: List[Func]) : Result[String, Stmt] {
+        
+    }
+
+    def checkStatementSemantic(stmt: Stmt) : Result[String, Stmt] {
+
+    }
+}
+
+object  CheckTypeSemantic {
+
+    def checkTypeSemantic(stmt: Stmt) : Result[String, Stmt] {
+
+    }
+}
+
+object Semantic {
+  //Array
+    def checkSemantic(prog: Program) : Result[String, Stmt] = {
+        checkStatementSemantic(prog.body)
+        prog.funcs.foreach(f => checkStatmentSemantic(f))
+    }
+
+
+
+// // def checkSemantic(expr: Expr, error: Error): Type = {
+// //   expr match {
+// //     case Var(_, varType) => varType
+// //     case ArrayLiteral(elements, elementType, isFrozen) =>
+// //       elements.foreach(checkSemantic(_, error))
+// //       ArrayType(elementType, isFrozen)
+// //     case ArrayAccess(arr, index) =>
+// //       val ArrayType(elementType, _) = checkSemantic(arr, error)
+// //       val indexType = checkSemantic(index, error)
+// //       if (indexType != IntType) {
+// //         error.addError("Array index must be of type Int.")
+// //       }
+// //       elementType
+// //     case ArrayModification(arr, index, newValue) =>
+// //       val ArrayType(elementType, isFrozen) = checkSemantic(arr, error)
+// //       val indexType = checkSemantic(index, error)
+// //       if (indexType != IntType) {
+// //         error.addError("Array index must be of type Int.")
+// //       }
+// //       val newValueType = checkSemantic(newValue, error)
+// //       if (isFrozen) {
+// //         error.addError("Cannot modify a frozen array.")
+// //       }
+// //       if (newValueType != elementType) {
+// //         error.addError("New value type does not match array element type.")
+// //       }
+// //       elementType
+// //   }
+// // }
 }
